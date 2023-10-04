79da14c89d76403ff9a7205b0f4ff031bd81abf2
Kurt Morris <kurt.morris@me.com>
2023-10-04 00:50:02 -0600

    Update runescape.sh for MESA Graphics users

--- resources/runescape.sh
+++ resources/runescape.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 systemd-run --user --service-type=forking \
+    --setenv='vblank_mode=0' \
+    --setenv='MESA_GL_VERSION_OVERRIDE=4.5FC' \
     --setenv=JX_ACCESS_TOKEN="$JX_ACCESS_TOKEN" \
     --setenv=JX_CHARACTER_ID="$JX_CHARACTER_ID" \
     --setenv=JX_DISPLAY_NAME="$JX_DISPLAY_NAME" \
