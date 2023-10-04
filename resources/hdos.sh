9f6be97df87b8a1649b48045615c672ebc26c7e1
Kurt Morris <kurt.morris@me.com>
2023-10-03 23:53:50 -0600

    Update hdos.sh for NixOS config.

--- resources/hdos.sh
+++ resources/hdos.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -e
-flatpak run --env=PULSE_LATENCY_MSEC=200 \
-    --env='vblank_mode=0' \
-    --env='MESA_GL_VERSION_OVERRIDE=4.5FC' \
-    dev.hdos.HDOS || \
-flatpak-spawn --host flatpak run \
-    --env=PULSE_LATENCY_MSEC=200 \
-    --env='vblank_mode=0' \
-    --env='MESA_GL_VERSION_OVERRIDE=4.5FC' \
+flatpak run \
     --env=JX_ACCESS_TOKEN="$JX_ACCESS_TOKEN" \
     --env=JX_CHARACTER_ID="$JX_CHARACTER_ID" \
     --env=JX_DISPLAY_NAME="$JX_DISPLAY_NAME" \
